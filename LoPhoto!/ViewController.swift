26cc4f1cb2d2d7979f696cf66488237c164e66fb
pRopia <pratik.ropia@gmail.com>
2019-08-22 22:59:44 +0200

    Initial Commit

---
+++ LoPhoto!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LoPhoto!
+//
+//  Created by Prateek Ropia on 22/08/2019.
+//  Copyright © 2019 Prateek Ropia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LoPhoto!Tests/LoPhoto_Tests.swift
@@ -0,0 +1,34 @@
+//
+//  LoPhoto_Tests.swift
+//  LoPhoto!Tests
+//
+//  Created by Prateek Ropia on 22/08/2019.
+//  Copyright © 2019 Prateek Ropia. All rights reserved.
+//
+
+import XCTest
+@testable import LoPhoto_
+
+class LoPhoto_Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
